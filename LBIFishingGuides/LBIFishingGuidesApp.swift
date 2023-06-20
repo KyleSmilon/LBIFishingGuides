13e3b1443aaf112539cce227350b0b3ee5d195d0
Kyle Smilon <kdsmilon@gmail.com>
2023-06-20 15:31:33 -0400

    initial

---
+++ LBIFishingGuides/LBIFishingGuidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBIFishingGuidesApp.swift
+//  LBIFishingGuides
+//
+//  Created by Kyle Smilon on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct LBIFishingGuidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
